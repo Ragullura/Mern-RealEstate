81bb1acb5bee153ff10bbcfadc44240d5094745a
Ragullura <ragullura.005@gmail.com>
2024-04-12 22:07:21 +0530

    create a sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-/* import { errorHandler } from '../utils/error.js';  */
-
-
+import { errorHandler } from '../utils/error.js';  
+import jwt from 'jsonwebtoken';
+//It's for signup page 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+       // check  if user already exists in the database
+      if(!username || !email || !password || username==='' || email ==='' || password===''){
+    /* return res.status(400).json({msg:'Please fill out all fields.'}); */
+    //added from errorhandler
+      next(errorHandler(400,"Please fill out all fields"));
+      }
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
@@ -14,3 +21,40 @@ export const signup = async (req, res, next) => {
       next(error);
     }
   };
+
+  /* ------------************-------------------- */
+  /* It's for sign in  */
+
+  export const signin =async (req,res,next) => {
+    const { email, password } =req.body;
+    if(!email || !password || email ==='' || password===''){
+      next(errorHandler(400,'All fields are required'));
+   }
+   try {
+    //it will find the valid email and store it 
+    const validUser =await User.findOne({email});
+
+    if(!validUser){
+      return next(errorHandler(404,'User not found'));
+   }
+    
+
+    //it will help us to compare the password and then hash the password
+    const validPassword =bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400,'Invalid password'));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //we dont want to see hash password also in database for that
+    const {password:pass, ...rest} =validUser._doc;
+    res.status(200).cookie('accress_token',token,{
+      httpOnly :true}).json(rest);
+
+    
+   } catch (error) {
+    next(error);
+   }
+  }
+
+  
