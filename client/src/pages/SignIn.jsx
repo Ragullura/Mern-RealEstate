9f3c4a56ea8e47ff1848bce911846cb45e1e4cd6
Ragullura <ragullura.005@gmail.com>
2024-04-13 15:59:06 +0530

    complete sign in page functionality and also add redux tookit for state management

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,61 @@
-import {  Button, Label, TextInput } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Alert,  Button, Label,Spinner, TextInput } from 'flowbite-react'
+import  { useState } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { Link , useNavigate} from 'react-router-dom';
 import family from '../assets/family.png';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+
 export default function SignIn() {
+  const [formData,setFormData] =useState({});
+  const {loading,error:errorMessage} =useSelector(state => state.user);
+   //why we  are using selector here? because we want to access 
+   const dispatch =useDispatch();
+   //useNavigate for navigate to sign in page
+  const navigate =useNavigate();
+
+
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id]:e.target.value.trim() })//who trims whitespaces at beginning and end of values
+  }
+
+  //handlesubmit and using async for handling promise
+  // because it take  time to submit data to the server
+  const handleSubmit =async (e) => {
+    e.preventDefault();
+    if( !formData.email || !formData.password){
+      return  dispatch(signInFailure("Please fill all fields!"));
+    }
+    try {
+      dispatch(signInStart());//dispatching action to store that we start to load the data
+      //here have some problem front end run in localhost 5173
+      //and back end run in localhost 3000 
+      //so add proxy in vite config 
+      const res = await fetch('/api/auth/signin',{
+        method:'POST',  
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(formData),
+      })
+      //convert the data 
+      const  data=await res.json();
+       //check same name  or email already exist
+       if(data.success===false){
+        dispatch(signInFailure(data.message));
+      }
+
+      //once all done successfully then navigate in sign-in page
+      if(res.ok){
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+
+    } catch (error) {
+      //This error for client side network issue 
+      dispatch(signInFailure(error.message));
+      
+    }
+  }
 
   return (
     <div className='min-h-screen mt-20'>
@@ -18,23 +72,39 @@ export default function SignIn() {
           </div>
           {/*  this is for right side */}
           <div className='flex-1'>
-              <form  className='flex flex-col gap-4' >
+              <form  className='flex flex-col gap-4' onSubmit={handleSubmit} method="post" >
               <div>
               <Label value='Email'></Label>
-              <TextInput  type='email' placeholder='xyz@gmail.com' id='email'  />
+              <TextInput  type='email' placeholder='xyz@gmail.com' onChange={handleChange} id='email'  />
               </div>
               <div >
               <Label value='Password'></Label>
-              <TextInput  type='password' placeholder='password' id='password'  />
+              <TextInput  type='password' placeholder='password' onChange={handleChange} id='password'  />
               </div>
-              <Button gradientDuoTone='purpleToPink' type='submit' >{/* disabled-- to avoid lot submision in signup */}
-              Sign In
+              <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading} >{/* disabled-- to avoid lot submision in signup */}
+              {
+               loading ? 
+               (
+                  <>
+                    <Spinner />
+                  <span className='pl-3'>Loading...</span>
+                  </>
+               ) :('Sign In')
+               }
+             
             </Button>
               </form>
               <div className='flex gap-2 text-sm mt-5'>
               <span>Dont Have an Account ?</span>
               <Link to='/sign-up' className='text-blue-500'>Sign up</Link>
               </div>
+              {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
            
           </div>
       </div>
