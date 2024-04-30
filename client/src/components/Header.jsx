f634dda4a97217a07284e5b0677a8ec624132879
Ragullura <ragullura.005@gmail.com>
2024-04-30 18:55:10 +0530

    update header and search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,81 +1,128 @@
-import { useEffect, useState } from 'react'
-import logo1 from '../assets/logo1.png'
-import { Link, useNavigate} from 'react-router-dom';
-import {Button, TextInput} from 'flowbite-react';
-import { AiOutlineSearch } from 'react-icons/ai';
-/* import { RiMenuFoldLine } from "react-icons/ri"; */
-import { useSelector } from 'react-redux';
-
+import { useEffect, useState } from "react";
+import logo1 from "../assets/logo1.png";
+import { Link, useNavigate } from "react-router-dom";
+import { Avatar, Button, TextInput, Dropdown } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import {
+  signoutSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('searchTerm', searchTerm);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
+    const searchTermFromUrl = urlParams.get("searchTerm");
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
+
+  /* Sign out function */
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <header className=' sticky top-0 z-30  bg-slate-200 shadow-lg w-full bg-opacity-80  backdrop-blur-sm'>
-      <div className='relative  flex justify-between items-center max-w-7xl mx-auto p-3 '>
-        <Link to='/' className='flex item gap-2 '>
-        <img src={logo1} alt='logo' className='w-9 h-9 object-contain ' />
-        <h1 className=' font-bold text-sm sm:text-xl flex flex-wrap  items-center '>
-          <span className='text-slate-600'>House&nbsp;</span>
-          <span className='text-green-500'>Hive</span>
-        </h1>
+    
+    <header   className="sticky top-0 z-30  bg-slate-200 shadow-lg w-full bg-opacity-80  backdrop-blur-sm">
+      <div className="relative  flex justify-between items-center max-w-7xl mx-auto p-3 ">
+        <Link to="/" className="flex item gap-2 ">
+          <img src={logo1} alt="logo" className="w-9 h-9 object-contain " />
+          <h1 className=" font-bold text-sm sm:text-xl flex flex-wrap  items-center ">
+            <span className="text-slate-600">House&nbsp;</span>
+            <span className="text-green-500">Hive</span>
+          </h1>
         </Link>
-        <form onSubmit={handleSubmit} className='flex items-center' >
-          <TextInput type="text" placeholder='Search...'
-          rightIcon={AiOutlineSearch} value={searchTerm}
-          onChange={(e)=>{setSearchTerm(e.target.value)}}
-          className='hidden lg:inline   cursor-pointer'
-           />
-        
+        <form onSubmit={handleSubmit} className="flex items-center">
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+            className="hidden lg:inline   cursor-pointer"
+          />
         </form>
-        <Link to='/search' >
-        <Button type='button'  className=' w-10 h-10 lg:hidden flex items-center justify-center ' color='gray'  pill><AiOutlineSearch className='cursor-pointer ' /></Button>
+        <Link to="/search">
+          <Button
+            type="button"
+            className=" w-10 h-10 lg:hidden flex items-center justify-center mx-auto"
+            color="gray"
+            pill
+          >
+            <AiOutlineSearch className="cursor-pointer " />
+          </Button>
         </Link>
+
+        <ul className="flex gap-4 items-center">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-500 transition duration-200">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:text-blue-500 transition duration-200">
+              About
+            </li>
+          </Link>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar alt="user" img={currentUser.profilePicture} rounded className=''/>
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.username}</span>
+                <span className="block text-sm font-medium truncate">
+                  {currentUser.email}
+                </span>
+              </Dropdown.Header>
+              <Link to={"/profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/sign-in">
+              <Button gradientDuoTone="purpleToBlue" outline>
+                Sign In
+              </Button>
+            </Link>
+          )}
         
-        <ul className='flex gap-4'>
-        <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:text-blue-500 transition duration-200'>
-            Home
-          </li>
-        </Link>
-        <Link to='/about' >
-          <li className='hidden sm:inline text-slate-700 hover:text-blue-500 transition duration-200'>
-            About
-          </li>
-        </Link>
-        <Link to='/profile' >
-        {currentUser ? (
-              <img
-                className='rounded-full h-7 w-7 object-cover'
-                src={currentUser.profilePicture}
-                alt='profile'
-              />
-            ) : (
-              <li className=' sm:inline text-slate-700 hover:text-blue-500 transition duration-200'>
-            Sign In
-          </li>
-            )}
-          
-        </Link>
-      </ul>
-      {/* <RiMenuFoldLine className=' w-6 h-6 text-slate-700' /> */}
+        </ul>
+        
       </div>
-      
+       
     </header>
-  )
+  );
 }
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -22,13 +22,13 @@ export default function Search() {
   /* -------------UseEffect --------------------- */
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    const typeFromUrl = urlParams.get('type');
-    const parkingFromUrl = urlParams.get('parking');
-    const furnishedFromUrl = urlParams.get('furnished');
-    const offerFromUrl = urlParams.get('offer');
-    const sortFromUrl = urlParams.get('sort');
-    const orderFromUrl = urlParams.get('order');
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
 
     if (
       searchTermFromUrl ||
@@ -40,13 +40,13 @@ export default function Search() {
       orderFromUrl
     ) {
       setSidebardata({
-        searchTerm: searchTermFromUrl || '',
-        type: typeFromUrl || 'all',
-        parking: parkingFromUrl === 'true' ? true : false,
-        furnished: furnishedFromUrl === 'true' ? true : false,
-        offer: offerFromUrl === 'true' ? true : false,
-        sort: sortFromUrl || 'created_at',
-        order: orderFromUrl || 'desc',
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
       });
     }
 
@@ -56,7 +56,8 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
-     
+      console.log("Fetched data:", data);
+      
       if (data.length > 8) {
         setShowMore(true);
       } else {
@@ -69,71 +70,69 @@ export default function Search() {
     fetchListings();
   }, [location.search]);
 
-
   /* handle  search bar */
   const handleChange = (e) => {
     if (
-      e.target.id === "all" ||
-      e.target.id === "rent" ||
-      e.target.id === "sale"
+      e.target.id === 'all' ||
+      e.target.id === 'rent' ||
+      e.target.id === 'sale'
     ) {
       setSidebardata({ ...sidebardata, type: e.target.id });
     }
 
-    if (e.target.id === "searchTerm") {
+    if (e.target.id === 'searchTerm') {
       setSidebardata({ ...sidebardata, searchTerm: e.target.value });
     }
 
     if (
-      e.target.id === "parking" ||
-      e.target.id === "furnished" ||
-      e.target.id === "offer"
+      e.target.id === 'parking' ||
+      e.target.id === 'furnished' ||
+      e.target.id === 'offer'
     ) {
       setSidebardata({
         ...sidebardata,
         [e.target.id]:
-          e.target.checked || e.target.checked === "true" ? true : false,
+          e.target.checked || e.target.checked === 'true' ? true : false,
       });
     }
 
-    if (e.target.id === "sort_order") {
-      const sort = e.target.value.split("_")[0] || "created_at";
+    if (e.target.id === 'sort_order') {
+      const sort = e.target.value.split('_')[0] || 'created_at';
 
-      const order = e.target.value.split(sort)[1] || "desc";
+      const order = e.target.value.split('_')[1] || 'desc';
 
       setSidebardata({ ...sidebardata, sort, order });
     }
   };
-
   /* ----------handle submit in form-------------- */
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams();
-    urlParams.set('searchTerm', sidebardata.searchTerm);
-    urlParams.set('type', sidebardata.type);
-    urlParams.set('parking', sidebardata.parking);
-    urlParams.set('furnished', sidebardata.furnished);
-    urlParams.set('offer', sidebardata.offer);
-    urlParams.set('sort', sidebardata.sort);
-    urlParams.set('order', sidebardata.order);
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
 
   /* ------Showmore------------- */
-  const onShowMoreClick = async () =>{
-    const numberOfListings =listings.length;
-    const startIndex =numberOfListings;
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
     const urlParams = new URLSearchParams(location.search);
-    urlParams.set('startIndex',startIndex);
-    const searchQuery =urlParams.toString();
-    const res =await fetch (`/api/listing/get?$filter=${searchQuery}`) ;
+    urlParams.set("startIndex", startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?$filter=${searchQuery}`);
 
-    const data =await res.json() ;
-    if(data.length < 9){
+    const data = await res.json();
+    if (data.length < 9) {
       setShowMore(false);
     }
-    setListings( [...listings , ...data ] );
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -255,28 +254,29 @@ export default function Search() {
           Listing Results :
         </h1>
         {/* -------Loading effect-------- */}
-        <div className='p-7 flex flex-wrap gap-4'> 
-
-        {!loading && listings.length === 0 && (
-            <p className='text-xl text-slate-700'>No listing found!</p>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
           )}
           {loading && (
-            <p className='text-xl text-slate-700 text-center w-full'>
+            <p className="text-xl text-slate-700 text-center w-full">
               Loading...
             </p>
           )}
-      
-       {!loading &&
+
+          {!loading &&
             listings &&
             listings.map((listing) => (
               <ListingItem key={listing._id} listing={listing} />
             ))}
-        {showMore && (
-          <button onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">
-            Show more..
-          </button>
-        )}
-
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className="text-green-700 hover:underline p-7 text-center w-full"
+            >
+              Show more..
+            </button>
+          )}
         </div>
       </div>
       {/* -----sec div end------ */}
